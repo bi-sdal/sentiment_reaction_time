a573e307e0fc6d14e3644c41572a1d897d6352ad
Daniel Chen <Maveri2k@yahoo.com>
2017-08-09 14:15:44 -0400

    look for files and drop columns that are completely missing

--- src/01-prep_data.R
+++ src/01-prep_data.R
@@ -5,8 +5,8 @@ rm(list = ls())
 
 LARGEST_ID <- 50
 
-sub_stats <- list.files(path = 'data', pattern = 'allAnonymizedSubjectStats*', full.names = TRUE)
-sub_actions <- list.files(path = 'data', pattern = 'allSubjectActionsOutput*', full.names = TRUE)
+sub_stats <- list.files(path = 'data', pattern = '*allAnonymizedSubjectStats*', full.names = TRUE)
+sub_actions <- list.files(path = 'data', pattern = '*allSubjectActionsOutput*', full.names = TRUE)
 
 id_data_l <- lapply(X = sub_stats, FUN = read_delim, delim = '\t', skip = 1,
                   col_names = c('uuid', 'var1', 'sub_num', 'var2', 'var3'))
@@ -18,6 +18,9 @@ df <- do.call(rbind, action_data_l)
 
 ids$id <- as.numeric(ids$sub_num)
 ids <- ids[ids$id < LARGEST_ID, ]
+
+missing_cols <- sapply(ids, function(x) all(is.na(x)))
+ids <- ids[, !missing_cols]
 ids <- ids[complete.cases(ids), ]
 
 data <- merge(x = df, y = ids, by.x = 'subjuuid', by.y = 'uuid')
